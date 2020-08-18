5b244f505465ae47e06bd5fef5ab2f84bc18dc35
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-18 08:39:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,19 @@
 def square_array(array)
   counter = 0 
   array = [1,2,3]
-  while array[counter] do
+  while counter < array.length do
     puts array[counter] ** 2 
     counter += 1
+  end
 end
-  
-  
+
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
